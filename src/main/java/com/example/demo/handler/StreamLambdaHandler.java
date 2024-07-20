7c9db82c0f334dc3ac1aee98287c9be4707f5240
taeyun1215 <taeyun1215@naver.com>
2024-07-20 16:07:07 +0900

    stream lambda handler

--- src/main/java/com/example/demo/handler/StreamLambdaHandler.java
+++ src/main/java/com/example/demo/handler/StreamLambdaHandler.java
@@ -6,18 +6,17 @@ import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.example.demo.UserApplication;
-import org.springframework.context.annotation.ComponentScan;
+import com.example.demo.DemoApplication;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class UserLambdaHandler implements RequestStreamHandler {
+public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     static {
         try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(UserApplication.class);
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DemoApplication.class);
             // If you are using HTTP APIs with the version 2.0 of the proxy model, use the getHttpApiV2ProxyHandler
             // method: handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
         } catch (ContainerInitializationException e) {
